c885fe36afadf616653409bc8a8e4b0929fff957
havyas <havyasagaltha@gmail.com>
2025-07-27 22:15:11 +0530

    made some chagnes

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import ReactDOM from "react-dom/client";
+
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from './components/Home';
 import Edit from './components/Edit';
